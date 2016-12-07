b9ff977f0fb2e2f61d0c9bdff2b8d322d131ee93
Dorothea Pfeiffer <dorothea.pfeiffer@esss.se>
2016-12-07 22:00:56 +0100

    Added parameters -tac and -bc to analyzeData.py

--- SRS/vmm2root/analyzeData.py
+++ SRS/vmm2root/analyzeData.py
@@ -1,13 +1,14 @@
 import os
 import subprocess
-directory=os.path.dirname(os.path.abspath(__file__))
-
+#directory=os.path.dirname(os.path.abspath(__file__))
+directory='/Users/dpfeiffe/data/vmm2TimingStudy'
 for filename in os.listdir(directory):
-    if filename.endswith(".raw") and filename.startswith("run_"): 
-        dataFileName = os.path.join(directory, filename)
-        print dataFileName
-        args = ['./rvmm2root', '-rd', dataFileName, '-tac', 125, '-bc', 2.5]
-        subprocess.call(args) 
-    else:
-        continue
+	print filename
+	if filename.endswith(".raw") and filename.startswith("run_"): 
+		dataFileName = os.path.join(directory, filename)
+		print dataFileName
+		args = ['./vmm2root', '-rd', dataFileName, '-tac', '125', '-bc', '2.5']
+		subprocess.call(args) 
+	else:
+		continue
 
