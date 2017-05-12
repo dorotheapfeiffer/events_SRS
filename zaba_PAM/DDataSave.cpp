14b130e5ca8a4573b8758790f4d7e532ac1a7c9c
francescomessi <francesco.messi@nuclear.lu.se>
2017-05-12 11:31:47 +0200

    added zaba_PAM, under development to acquire more V1740 with synchronous CLK and TimeStamp reset

---
+++ zaba_PAM/DDataSave.cpp
@@ -0,0 +1,14 @@
+#include "DDataSave.h"
+
+  ClassImp(DDataSave)
+
+//-----------------------------------------------------------------------------
+ DDataSave::DDataSave(DAcquisition *fMultiGrid) {
+  std::cout << "[MESSAGE] Constructing DataSave" << std::endl; //ja
+
+}
+//-----------------------------------------------------------------------------
+ DDataSave::~DDataSave() {
+
+}
+//-----------------------------------------------------------------------------
